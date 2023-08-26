02a745b16d86d0eaeedca5755667e42839c64296
Shivam Srivastava <80268511+Srivastava28@users.noreply.github.com>
2023-08-26 11:35:00 +0530

    Create CheckIfArrayIsSortedAndRotated.cpp

---
+++ ArrayEasy/CheckIfArrayIsSortedAndRotated.cpp
@@ -0,0 +1,22 @@
+// Question Link: https://leetcode.com/problems/check-if-array-is-sorted-and-rotated/description/
+
+
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        int count = 0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]>nums[(i+1)%nums.size()])
+            {
+                count++;
+            }
+        }
+        if(count<=1)
+        {
+            return true;
+        }
+        else return false;
+
+    }
+};
